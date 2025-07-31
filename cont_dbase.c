308e8f38786df7292b5baf8f0c4ca6f112d38b81
Sfinkymvp <sfinkymvp@gmail.com>
2025-07-31 20:32:17 +0300

    Added import/export, improved interface

--- cont_dbase.c
+++ cont_dbase.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 
+//  Максимальное количество контактов в базе данных
+#define CONTACT_LIM 100 
 //  Максимальная длина введенных пользователем данных
 #define CHAR_LIM 50
-//  Максимальное количество контактов в книге
-#define CONTACT_LIM 100 
 
 
 //  Структура описывает имя, фамилию, телефон и email
@@ -17,8 +18,8 @@ typedef struct {
 } Contact;
 
 
-//  Запуск программы книги контактов
-void contact_book(Contact *data, int len);
+//  Запуск программы базы данных контактов
+void contact_database(Contact *data, int len);
 
 
 //  Стартовая фраза
@@ -35,7 +36,7 @@ void enter_menu(char *answer, int len);
 int select_item(Contact *data, int len);
 
 
-//  Добавление контакта в книгу
+//  Добавление контакта в базу данных
 void add_contact(Contact *data, int len);
 //  Ввод контакта пользователем
 void input_new_contact(Contact *data, int len);
@@ -51,6 +52,8 @@ void print_contact(Contact person);
 int find_contact_index(Contact *data, int len, char *name1, char *name2);
 //  Очистка буфера ввода
 void clear_buffer();
+//  Очистка терминала
+void clear_screen();
 //  Ввод пользователем имени и фамилии контакта
 void enter_full_name(char *name1, char *name2);
 //  Удаление контакта по переданному индексу
@@ -71,18 +74,36 @@ int name_comparison(Contact el, Contact key);
 void insertion_sort_for_contacts(Contact *data, int len);
 
 
+//  Экспорт контактов
+void save_contacts(Contact *data, int len);
+//  Переносит контакты в файл
+void transfer_contacts(Contact *data, int len, char *file);
+//  Ввод пользователем файла, в который будет произведено сохранение
+void enter_file(char *file, int max_name_len);
+
+
+//  Импорт контактов
+void load_contacts(Contact *data, int len);
+//  Переносит контакты из файла
+void loading_contacts(Contact *data, int len, char *file);
+//  Проверяет, существует ли файл или нет
+int file_exists(char *file);
+
+
 int main()
 {
 	Contact data[CONTACT_LIM] = {0};
 	
-	contact_book(data, CONTACT_LIM);
+	contact_database(data, CONTACT_LIM);
 	
 	return 0;
 }
 
 
-void contact_book(Contact *data, int len)
+void contact_database(Contact *data, int len)
 {
+	clear_screen();
+	
 	introductory_phrase();
 	
 	selection_menu(data, len);
@@ -100,7 +121,7 @@ void introductory_phrase()
 
 void last_phrase()
 {
-	printf("\nSee you later. Bye!\n");
+	printf("See you later. Bye!\n");
 }
 
 
@@ -111,7 +132,9 @@ void selection_menu(Contact *data, int len)
 	printf("2 - Show all contacts\n");
 	printf("3 - Find contact\n");
 	printf("4 - Delete contact\n");
-	printf("5 - Exit\n\n");
+	printf("5 - Exit\n");
+	printf("6 - Save contacts to file\n");
+	printf("7 - Load contacts from file\n\n");
 	} while (select_item(data, len) == 0);
 }
 
@@ -121,9 +144,9 @@ void enter_menu(char *answer, int len)
 	fgets(answer, len, stdin);
 	
 	while (strlen(answer) != 2 || 
-		!('1' <= answer[0] && answer[0] <= '5')) {
+		!('1' <= answer[0] && answer[0] <= '7')) {
 			
-		printf("Wrong answer, try again\n");
+		printf("\nWrong answer, try again\n");
 		
 		fgets(answer, len, stdin);
 	}
@@ -135,6 +158,8 @@ int select_item(Contact *data, int len)
 	char answer[CHAR_LIM] = {0};
 	
 	enter_menu(answer, sizeof(answer));
+
+	clear_screen();
 	
 	switch (answer[0]) {
 		case '1': add_contact(data, len); break;
@@ -142,6 +167,8 @@ int select_item(Contact *data, int len)
 		case '3': find_contact(data, len); break;
 		case '4': delete_contact(data, len); break;
 		case '5': return 1;
+		case '6': save_contacts(data, len); break;
+		case '7': load_contacts(data, len); break;
 	}
 	
 	return 0;
@@ -166,11 +193,14 @@ void input_new_contact(Contact *data, int len)
 		k = i + 1;
 		
 	if (k >= len) {
-    printf("The contact book is full!\n");
-    return;
+	    printf("\nThe contact book is full!\n");
+	    return;
 	}
 		
-	scanf("%s %s %s %s", data[k].firstname, data[k].lastname, data[k].phone, data[k].email);
+	scanf("%s %s %s %s", data[k].firstname,
+						 data[k].lastname,
+						 data[k].phone,
+						 data[k].email);
 	
 	clear_buffer();
 }
@@ -178,7 +208,7 @@ void input_new_contact(Contact *data, int len)
 
 void display_all_contacts(Contact *data, int len)
 {
-	printf("\nFirstname  Lastname  Phone  Email");
+	printf("Firstname  Lastname  Phone  Email");
 	printf("\n---------------------------------\n");
 	
 	for (int i = 0; i < len; i++) {
@@ -193,7 +223,10 @@ void display_all_contacts(Contact *data, int len)
 
 void print_contact(Contact person)
 {
-	printf("%s %s %s %s\n", person.firstname, person.lastname, person.phone, person.email);
+	printf("%s %s %s %s\n", person.firstname,
+							person.lastname,
+							person.phone,
+							person.email);
 }
 
 
@@ -215,6 +248,15 @@ void clear_buffer()
 {
 	for (int c = getchar(); c != '\n' && c != EOF; c = getchar())
 		;
+}
+
+
+void clear_screen() {
+#ifdef _WIN32
+    system("cls");    // Windows
+#else
+    system("clear");  // Linux / Mac
+#endif
 }
 
 
@@ -320,3 +362,99 @@ void insertion_sort_for_contacts(Contact *data, int len)
 		data[k + 1] = key;
 	}
 }
+
+
+void save_contacts(Contact *data, int len)
+{
+	char file[CHAR_LIM] = {0};
+	
+	enter_file(file, sizeof(file));
+
+	transfer_contacts(data, len, file);
+}
+
+
+void transfer_contacts(Contact *data, int len, char *file)
+{
+	FILE * fout = fopen(file, "w");
+
+	if (!fout) {
+		printf("\nFailed to write data to the specified file\n");
+		return;
+	}
+
+	for (int i = 0; i < len; i++) {
+		if (data[i].firstname[0] != '\0') {
+			fprintf(fout, "%s %s %s %s\n", data[i].firstname,
+										   data[i].lastname,
+										   data[i].phone,
+										   data[i].email);
+		}
+	}
+
+	fclose(fout);
+
+	printf("\nSave successful\n");
+}
+
+
+void enter_file(char *file, int max_name_len)
+{
+	printf("Enter the name of the file\n\n");
+	
+	fgets(file, max_name_len, stdin);
+	
+	file[strcspn(file, "\n")] = '\0';
+}
+
+
+void load_contacts(Contact *data, int len)
+{
+	char file[CHAR_LIM] = {0};
+
+	enter_file(file, sizeof(file));
+
+	if (!file_exists(file)) {
+		printf("\nFile not found for import\n");
+		return;
+	}
+
+	loading_contacts(data, len, file);
+
+	printf("\nContacts added\n");
+}
+
+
+void loading_contacts(Contact *data, int len, char *file)
+{
+	FILE * fin = fopen(file, "r");
+
+	for (int i = 0; i < len; i++) {
+		if (data[i].firstname[0] != '\0')
+			continue;
+
+		int count = fscanf(fin, "%s %s %s %s", data[i].firstname,
+											   data[i].lastname, 
+											   data[i].phone,
+											   data[i].email);
+											   
+		if (count != 4)
+			break;						
+	}
+
+	fclose(fin);
+}
+
+
+int file_exists(char *file)
+{
+	FILE * fcheck = fopen(file, "r");
+
+	if (!fcheck)
+		return 0;
+
+	fclose(fcheck);
+	return 1;
+}
+
+
